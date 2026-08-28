320016f08ead946dad814b893473deb272e0f9b2
NadaAtef <nada.atef5520@gmail.com>
2026-08-28 23:06:58 +0300

    intialize calculator

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Simulation.Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number : ");
+            int b = int.Parse(Console.ReadLine());
+
+            
+
+        }
+    }
+}
